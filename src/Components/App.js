703eee8fea3912af4cf7ae04d4102a3fa2ece886
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-24 13:16:56 -0400

    ADD: Tabla de estadisticas

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 
 import MediaPlayer from "./MediaPlayer";
 import Navbar from "./Navbar";
+import StadisticTable from "./StadisticsTable";
 
 function App() {
   return (
     <Fragment>
       <MediaPlayer />
       <Navbar />
+      <StadisticTable />
     </Fragment>
   );
 }
---
+++ src/Components/StadisticsTable.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./css/StadisticTable.css";
+import NavbarTable from "./NavbarTable";
+import Stadistics from "./Stadistics";
+
+function StadisticTable() {
+  return (
+    <div class="StadisticTable">
+      <NavbarTable />
+      <Stadistics />
+    </div>
+  );
+}
+
+export default StadisticTable;
