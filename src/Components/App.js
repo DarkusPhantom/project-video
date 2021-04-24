4fcf6b58b3a81a0450f893b9808edd4628076f3c
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-24 11:11:45 -0400

    ADD: Componente Navbar
    - Se coloco los estilos de los botones en general a index.css
    - Estilos al Navbar
    - Estructura al Navbar
    - Renombrando algunas clases de los botones para hacerlo general y reutilizable

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 
 import MediaPlayer from "./MediaPlayer";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <Fragment>
       <MediaPlayer />
+      <Navbar />
     </Fragment>
   );
 }
--- src/Components/BtnVideo.js
+++ src/Components/BtnVideo.js
@@ -7,10 +7,10 @@ import HeartBroken from "../Images/Icons/heart-broken.svg";
 const BtnVideo = () => {
   return (
     <div className="controls">
-      <a className="btn like">
+      <a className="btn btn-reaction like">
         <img src={Heart} alt="Heart" />
       </a>
-      <a className="btn dislike">
+      <a className="btn btn-reaction dislike">
         <img src={HeartBroken} alt="Heart Broken" />
       </a>
       <a className="btn more-info">More Info</a>
