19793de150fed253a61e98a91cba7eb9f6ee1bfd
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-24 23:35:15 -0400

    REFACTOR: Cambio en los btn del Navbar Stadistic
    - Se le agrego un boton para ver mas opciones en el boton de generacion
    - Reestructuracion de los botones para ser reutilizables

--- src/Components/NavbarTable.js
+++ src/Components/NavbarTable.js
@@ -6,8 +6,27 @@ function NavbarTable() {
   return (
     <div class="navbar navbar-sort">
       <p className="navbar-text">Ordenar por:</p>
-      <a class="btn btn-option">Generacion</a>
-      <a class="btn btn-option">Por Semana</a>
+      <div className="btn btn-option">
+        <a>Generacion</a>
+        <div className="options">
+          <span className="options-symb">*</span>
+          <ul className="options-list">
+            <li className="option">
+              <a>Milenials</a>
+            </li>
+            <li className="option">
+              <a>Baby Boomer</a>
+            </li>
+            <li className="option">
+              <a>Gen X</a>
+            </li>
+            <li className="option">
+              <a>Gen Y</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <a class="btn btn-option">Semanal</a>
     </div>
   );
 }
